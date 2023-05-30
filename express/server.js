3f1516047b9354362093b8bcab05b351a27604a0
Leonardo Jaques <103287079+LeoJaques@users.noreply.github.com>
2023-05-30 11:51:53 -0400

    aula 20

--- express/server.js
+++ express/server.js
@@ -1,31 +1,29 @@
-const express = require("../node_modules/express")
+const express = require('express')
 const app = express()
+const handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars') 
+const { Sequelize, DataTypes } = require('sequelize')
 
+
+//CONFIG----------------------------------------------------
+
+//TEMPLATE ENGINE
+app.engine('handlebars', expressHandlebars.engine)
+app.set('view engine', 'handlebars')
+
+// DATABASE CONNECTION
+const sequelize = new Sequelize('teste', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+
+//routers
+app.get('/cad', (req, res) => {
+    res.send('kk ai man')
+})
 
 
 const PORT = 3000
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
-
-app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/public/about.html")
-})
-
-app.get("/contact", (req, res) => {
-    res.sendFile(__dirname + "/public/contact.html")
-})
-
-app.get("/services", (req, res) => {
-    res.send("Welcome, services page")
-})
-
-
-
-
-app.listen(PORT, ()=> console.log(`Server running at ${PORT}`)) // dever ser a ultima linha
-
-
-// https://www.youtube.com/watch?v=u3MrPxq_RyA&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=9
-// instalando nodemon
+app.listen(PORT, () => console.log(`Server running at ${PORT}`)) // dever ser a ultima linha
--- express/teste.js
+++ express/teste.js
@@ -1,7 +1,54 @@
-const Sequelize = require('../node_modules/sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize('teste', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql'
 })
 
+
+const postagem = sequelize.define('postagem', {
+  titulo: {
+    type: DataTypes.STRING
+  },
+  conteudo: {
+    type: DataTypes.TEXT
+  },
+  autor: {
+    type: DataTypes.STRING
+  }
+})
+
+
+const user = sequelize.define('usuario', {
+  nome: {
+    type: DataTypes.STRING
+  },
+  sobrenome: {
+    type: DataTypes.STRING
+  },
+  idade: {
+    type: DataTypes.INTEGER
+  },
+  email: {
+    type: DataTypes.STRING
+  }
+})
+
+
+// postagem.sync()
+// user.sync()
+
+
+await user.create({
+  nome: "Leonardo",
+  sobrenome: "Jaques",
+  idade: 22,
+  email: "leonardo.jaquesdev@outlook.com"
+})
+
+
+await postagem.create({
+  titulo: "Blog com node",
+  conteudo: "lorem ipsum",
+  autor: "Leonardo Jaques"
+})
 
